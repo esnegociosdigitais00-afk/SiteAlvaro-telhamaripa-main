a202492f073ca63e57bfe724e994992bd768bf82
[dyad] <emersonsantosp96@gmail.com>
2026-01-22 19:19:51 -0300

    [dyad] Reordenando categorias de produtos - wrote 1 file(s)

--- src/components/CategorySection.tsx
+++ src/components/CategorySection.tsx
@@ -12,8 +12,8 @@ interface Category {
 const categories: Category[] = [
   { name: "Telha Térmica trapezoidal Confort", slug: "telha-termica-trapezoidal-confort", image: "/isotelha-trapezoidal.png" },
   { name: "Telha Metálica Sanduiche", slug: "telha-metalica-sanduiche", image: "/telha-metalica-sanduiche.png" },
-  { name: "Calhas e rufos", slug: "calhas-e-rufos", image: "/calhas-e-rufos.png" },
   { name: "Telha Galvalume Simples trapezoidal", slug: "telha-galvalume-simples-trapezoidal", image: "/telha-galvalume-simples.png" },
+  { name: "Calhas e rufos", slug: "calhas-e-rufos", image: "/calhas-e-rufos.png" },
 ];
 
 const CategoryCard: React.FC<{ category: Category }> = ({ category }) => {
