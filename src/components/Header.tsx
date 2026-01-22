027635618f7fdc8babdde55a85a3c28998fd6713
[dyad] <emersonsantosp96@gmail.com>
2026-01-22 18:52:08 -0300

    [dyad] Aumentando ainda mais o logo - wrote 1 file(s)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,8 +12,8 @@ const Logo = () => (
     <img 
       src="/logo_isotelhas_rj_new_v2.png" 
       alt="Isotelhas RJ - Conforto que Protege" 
-      // Altura responsiva: h-20 em mobile, h-16 em telas grandes para um logo maior
-      className="h-20 lg:h-16 w-auto" 
+      // Altura responsiva: h-20 em mobile, h-24 em telas grandes para um logo bem maior
+      className="h-20 lg:h-24 w-auto" 
     />
   </Link>
 );
