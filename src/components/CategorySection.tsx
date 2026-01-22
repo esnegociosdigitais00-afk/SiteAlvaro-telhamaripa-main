a7d8427e0c46fbd9ea6566a1e9433422eb56bb58
[dyad] <emersonsantosp96@gmail.com>
2026-01-22 19:24:25 -0300

    [dyad] Atualizando nome de categoria - wrote 1 file(s)

--- src/components/CategorySection.tsx
+++ src/components/CategorySection.tsx
@@ -11,7 +11,7 @@ interface Category {
 
 const categories: Category[] = [
   { name: "Telha Térmica trapezoidal Confort", slug: "telha-termica-trapezoidal-confort", image: "/isotelha-trapezoidal.png" },
-  { name: "Telha Metálica Sanduiche", slug: "telha-metalica-sanduiche", image: "/telha-metalica-sanduiche.png" },
+  { name: "Telha térmica trapezoidal Sanduiche", slug: "telha-metalica-sanduiche", image: "/telha-metalica-sanduiche.png" },
   { name: "Telha Galvalume Simples trapezoidal", slug: "telha-galvalume-simples-trapezoidal", image: "/telha-galvalume-simples.png" },
   { name: "Calhas e rufos", slug: "calhas-e-rufos", image: "/calhas-e-rufos.png" },
 ];
